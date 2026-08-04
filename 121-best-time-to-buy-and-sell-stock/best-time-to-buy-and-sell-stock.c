1b2b55b358238898aa5cdfd1aa69a8d825bd5741
Akshita Kar <akshitakar07@gmail.com>
2026-08-04 13:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (98.84%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,13 @@
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProf = 0;
+    
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProf) {
+            maxProf = prices[i] - minPrice;
+        }
+    }
+    return maxProf;
+}
