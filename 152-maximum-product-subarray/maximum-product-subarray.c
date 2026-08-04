ce48468ba55760837baaa42be6a7b3d858b0b40d
Akshita Kar <akshitakar07@gmail.com>
2026-08-04 13:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (72.92%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.c
@@ -0,0 +1,18 @@
+int maxProduct(int* nums, int numsSize) {
+    int maxProd = nums[0];
+    int minProd = nums[0];
+    int result = nums[0];
+    
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] < 0) {
+            int temp = maxProd;
+            maxProd = minProd;
+            minProd = temp;
+        }
+        maxProd = (nums[i] > maxProd * nums[i]) ? nums[i] : maxProd * nums[i];
+        minProd = (nums[i] < minProd * nums[i]) ? nums[i] : minProd * nums[i];
+        
+        if (maxProd > result) result = maxProd;
+    }
+    return result;
+}
